84cdb14b4a51af5f56087a281c0dd47253b7db0a
Shubham Mahaseth <mahasethshubham@gmail.com>
2022-08-30 13:30:50 +0530

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import "../node_modules/materialize-css/dist/css/materialize.min.css";
+  import "../node_modules/materialize-css/dist/js/materialize.min.js";
+  import NavBar from './layout/NavBar.svelte';
+  import Home from './pages/Home.svelte';
+  import About from './pages/About.svelte';
+	
+</script>
+
+<Router>
+	<NavBar/>
+	<div class='container'>
+		<Route path="/" component={Home} />
+		<Route path="/about" component={About} />
+	</div>
+</Router>
+
